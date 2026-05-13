ba2561b4a2623b361acd5e8a6c6f8e8be29f1b17
Benjamin Wishart <benjamin.wishart@gmail.com>
2026-05-13 14:55:02 -0400

    Update models

--- src/main/java/com/dotohtwo/search/model/Reviewable.java
+++ src/main/java/com/dotohtwo/search/model/Reviewable.java
@@ -1,3 +1,44 @@
 package com.dotohtwo.search.model;
 
-public record Reviewable(String id, String title) {}
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.List;
+
+@Document(indexName = "reviewables")
+public class Reviewable {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Keyword)
+    private String type;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private List<String> tags;
+
+    public Reviewable() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public List<String> getTags() { return tags; }
+    public void setTags(List<String> tags) { this.tags = tags; }
+}
--- src/main/java/com/dotohtwo/search/model/User.java
+++ src/main/java/com/dotohtwo/search/model/User.java
@@ -1,3 +1,30 @@
 package com.dotohtwo.search.model;
 
-public record User(String id, String username) {}
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Keyword)
+    private String username;
+
+    @Field(type = FieldType.Text)
+    private String displayName;
+
+    public User() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getDisplayName() { return displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+}
